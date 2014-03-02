5f9eaf7ee885383eebeef89af61c0f5d32a754c8
josue54 <jraabarca@gmail.com>
2014-03-02 13:36:55 -0600

    optimizado el codigo

--- lectorc.php
+++ lectorc.php
@@ -3,6 +3,7 @@
 //$conexion = mysqli_connect("localhost", "admin", "12345","example");
 $aux=$argv[1];//!isset($argv[1]){die"print"}
 $primeravez=true;
+$columnames=array();
 $consulta="insert into students (nombre,apellido,correo,telefono,cedula) values";
 $valores=" ";
 $usoconsulta=FALSE;
@@ -19,6 +20,8 @@ if (($gestor = fopen($aux, "r")) !== FALSE) {
         $numero = count($datos);
          
          for ($c=0; $c < $numero-1; $c++) {
+
+            $stringconsulta=$consulta;
             $aux2=$datos[$c];
             $aux3=$datos[$c+1];
             $aux4=$datos[$c+2];
@@ -28,10 +31,15 @@ if (($gestor = fopen($aux, "r")) !== FALSE) {
             $consulta.=" ".$valores;
             echo $consulta;
             $usoconsulta=true;
-         // $resEmp = mysqli_query($conexion,$consulta) or die(mysqli_error($conexion));
-            $consulta=$stringconsulta;
-            $valores=" ";
-            break; 
+
+           // $resEmp = mysqli_query($conexion,$consulta) or die(mysqli_error($conexion));
+            
+         if($usoconsulta==true){
+        $consulta=$stringconsulta;
+        $usoconsulta=FALSE;
+        $valores=" ";
+        }
+        break; 
 }
          
     }
